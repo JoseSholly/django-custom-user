54b509229c8e39874615098474befba67b620bfb
JoseSholly <josepholawuyi30@gmail.com>
2023-09-20 00:58:51 +0100

    customuser subclass abstractbaseuser

--- custom_user/users/models.py
+++ custom_user/users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+'''
+Here, we:
+
+Created a new class called CustomUser that subclasses AbstractBaseUser
+Added fields for email, is_staff, is_active, and date_joined
+Set the USERNAME_FIELD -- which defines the unique identifier for the User model -- to email
+Specified that all objects for the class come from the CustomUserManager
+'''
